7558a4e465b8ccad7ead97ede5b59a5d66f88f68
mvVNN <123576502+mvVNN@users.noreply.github.com>
2026-01-23 10:02:17 +0100

    Implement interactive menu for scheduling system
    Added a menu for user interaction with options to plan appointments, display clients, and save data.

--- Main.java
+++ Main.java
@@ -9,29 +9,76 @@ public class Main {
 
         // ###########Création de l'établissement ###########
         Etablissement e = new Etablissement("LavageDesMs", 50);
-        
-        // ##################################
-        // Lancement du 1er scénario souris
-        //###################################
+                Scanner sc = new Scanner(System.in);
 
-        e.planifier();
+        int choix = -1;
 
-        // ###########################################        
-        // Lancement du 2eme: client existant et RDV Sale
-        e.planifier();
-        // ###########################################
+        while (choix != 0) {
+            // ###########Menu  ###########
+            System.out.println("\n########### MENU ###########");
+            System.out.println("1- Planifier un rendez-vous");
+            System.out.println("2- Afficher tous les clients");
+            System.out.println("3- Afficher le planning d'un jour");
+            System.out.println("4- Afficher les rendez-vous d'un client");
+            System.out.println("5- Sauvegarder les donnees");
+            System.out.println("0- Quitter");
+            System.out.print("choix : ");
 
+            try {
+                choix = Integer.parseInt(sc.nextLine());
+            } catch (Exception e1) {
+                choix = -1;
+            }
 
-        // ############################################
-        // Sauvegarde pour vérifier les fichiers
-        //############################################
-        
-        e.versFichierClients("clients.txt");
-        e.versFichierRendezVous("rdv.txt");
+            switch (choix) {
 
-        System.out.println("\n FIN du TEST");
+                case 1:
+                    e.planifier();
+                    break;
+
+                case 2:
+                    e.afficherClients();
+                    break;
+
+                case 3:
+                    System.out.print("Date (yyyy-mm-dd) : ");
+                    try {
+                        LocalDate d = LocalDate.parse(sc.nextLine());
+                        e.afficherPlanningJour(d);
+                    } catch (Exception ex) {
+                        System.out.println("Date invalide.");
+                    }
+                    break;
+
+                case 4:
+                    System.out.print("Numero du client : ");
+                    try {
+                        int num = Integer.parseInt(sc.nextLine());
+                        e.afficherRdvClient(num);
+                    } catch (Exception ex) {
+                        System.out.println("Numero invalide.");
+                    }
+                    break;
+
+                case 5:
+                    e.versFichierClients("clients.txt");
+                    e.versFichierRendezVous("rdv.txt");
+                    System.out.println("Sauvegarde effectuee.");
+                    break;
+
+                case 0:
+                    e.versFichierClients("clients.txt");
+                    e.versFichierRendezVous("rdv.txt");
+                    System.out.println("Fin du programme.");
+                    break;
+
+                default:
+                    System.out.println("Choix invalide.");
+            }
+        }
     }
 }
+
 
 
 
